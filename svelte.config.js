4ccabf826c0f114d6786bae17958dbf01ef68c28
RobolabGs2 <slavikelk@yandex.ru>
2023-04-28 03:15:54 +0300

    Fix build

--- svelte.config.js
+++ svelte.config.js
@@ -18,11 +18,11 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: null,
 			precompress: false,
 			strict: true,
 			paths: {
-				base: dev ? '' : '/your-repo-name'
+				base: dev ? '' : '/memaker'
 			}
 		})
 	}
